7d8af896bf3595915cfdad05f97e56d53d732dc8
Fahim1272 <imfahim72@gmail.com>
2022-10-27 03:00:18 +0600

    register auth done

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
+import app from '../../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
+const auth = getAuth(app);
 const Register = () => {
+    const [error, SetError] = useState('');
+    const [success, setSuccess] = useState(false)
     const handleRegister = event => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
-
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setSuccess(true)
+            })
+            .catch(error => {
+                console.error('error', error);
+                SetError(error.message)
+            })
     }
     return (
         <div style={{ height: '100%' }}>
@@ -24,16 +38,18 @@ const Register = () => {
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" name='email' placeholder="Enter email" />
+                            <Form.Control type="email" name='email' placeholder="Enter email" required />
 
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" name='password' placeholder="Password" />
+                            <Form.Control type="password" name='password' placeholder="Password" required />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                         </Form.Group>
+                        <p className='text-danger' >{error}</p>
+                        {success && <p className='text-info' > User Created Successfully</p>}
                         <Button variant="primary" type="submit">
                             Register
                         </Button>
